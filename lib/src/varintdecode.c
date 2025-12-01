4dda5e3036b9b8c3a149f20dd1fa7adb277a58a7
Marco <vogel.marco@mailbox.org>
2025-12-01 10:10:56 +0100

    added tables

--- lib/src/varintdecode.c
+++ lib/src/varintdecode.c
@@ -1,4 +1,5 @@
 #include "libvarintrvv.h"
+#include "utils.h"
 
 uint32_t varint_decode(uint8_t *input, uint32_t *output, size_t length)
 {
@@ -6,13 +7,23 @@ uint32_t varint_decode(uint8_t *input, uint32_t *output, size_t length)
 
     const vuint8m1_t varint_vec = __riscv_vle8_v_u8m1(input, vlmax_e8m1);
 
-    // convert vuint32m1_t -> vuint8m1_t -> vint8m1_t
     vint8m1_t vec_i8 = __riscv_vreinterpret_v_u8m1_i8m1(varint_vec);
 
-    // signed comparison with 0 checks if MSB is set
     vbool8_t mask = __riscv_vmslt_vx_i8m1_b8(vec_i8, 0, vlmax_e8m1);
 
     vuint8m1_t mask_as_u8 = __riscv_vreinterpret_v_b8_u8m1(mask);
-    vuint32m1_t mask_as_u32 = __riscv_vreinterpret_v_u8m1_u32m1(mask_as_u8);
-    return __riscv_vmv_x_s_u32m1_u32(mask_as_u32);
+    vuint64m1_t mask_as_u64 = __riscv_vreinterpret_v_u8m1_u64m1(mask_as_u8);
+    uint64_t result = __riscv_vmv_x_s_u64m1_u64(mask_as_u64);
+
+    uint32_t low_12_bits = result & 0xFFF;
+    // combine index and bytes consumed into a single lookup
+    index_bytes_consumed combined = combined_lookup[low_12_bits];
+    uint64_t consumed = combined.bytes_consumed;
+    uint8_t index = combined.index;
+
+    printf("consumed %ld index %d\n", consumed, index);
+
+    vuint8m1_t vectors = __riscv_vle8_v_u8m1(&vectorsrawbytes[index], vlmax_e8m1);
+
+    return (uint32_t)result;
 }
