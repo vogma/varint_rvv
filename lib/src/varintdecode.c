24aa819db306aebf0d7a157539d9e8d35c217029
vogma <vogel.marco@mailbox.org>
2025-12-03 17:53:28 +0100

    decoding working

--- lib/src/varintdecode.c
+++ lib/src/varintdecode.c
@@ -6,6 +6,7 @@ uint32_t varint_decode(uint8_t *input, uint32_t *output, size_t length)
     const size_t vlmax_e8m1 = __riscv_vsetvlmax_e8m1();
     const size_t vlmax_e16m1 = __riscv_vsetvlmax_e16m1();
     const size_t vlmax_e32m1 = __riscv_vsetvlmax_e32m1();
+    const size_t vlmax_e64m1 = __riscv_vsetvlmax_e64m1();
 
     const vuint8m1_t varint_vec = __riscv_vle8_v_u8m1(input, vlmax_e8m1);
 
@@ -15,9 +16,9 @@ uint32_t varint_decode(uint8_t *input, uint32_t *output, size_t length)
 
     vuint8m1_t mask_as_u8 = __riscv_vreinterpret_v_b8_u8m1(mask);
     vuint64m1_t mask_as_u64 = __riscv_vreinterpret_v_u8m1_u64m1(mask_as_u8);
-    uint64_t result = __riscv_vmv_x_s_u64m1_u64(mask_as_u64);
+    uint64_t mask_result = __riscv_vmv_x_s_u64m1_u64(mask_as_u64);
 
-    uint32_t low_12_bits = result & 0xFFF;
+    uint32_t low_12_bits = mask_result & 0xFFF;
     // combine index and bytes consumed into a single lookup
     index_bytes_consumed combined = combined_lookup[low_12_bits];
     uint64_t consumed = combined.bytes_consumed;
@@ -25,7 +26,7 @@ uint32_t varint_decode(uint8_t *input, uint32_t *output, size_t length)
 
     printf("consumed %ld index %d\n", consumed, index);
 
-    vint8m1_t vectors = __riscv_vle8_v_i8m1(&vectorsrawbytes[index*16], 128/8);
+    vint8m1_t vectors = __riscv_vle8_v_i8m1(&vectorsrawbytes[index * 16], 128 / 8);
 
     uint8_t ints_read = 0;
 
@@ -33,15 +34,7 @@ uint32_t varint_decode(uint8_t *input, uint32_t *output, size_t length)
     {
         ints_read = 6;
 
-        // uint8_t debug_vals[16];
-
-        // __riscv_vse8_v_i8m1(debug_vals,  vectors, vlmax_e8m1);
-        // printf("index: %02x %02x %02x %02x %02x %02x %02x %02x\n", debug_vals[0], debug_vals[1], debug_vals[2], debug_vals[3], debug_vals[4], debug_vals[5], debug_vals[6], debug_vals[7]);
-
         vuint8m1_t shuffled = __riscv_vrgather_vv_u8m1(varint_vec, __riscv_vreinterpret_v_i8m1_u8m1(vectors), vlmax_e8m1);
-
-        // __riscv_vse8_v_u8m1(debug_vals, shuffled, vlmax_e8m1);
-        // printf("shuffled: %02x %02x %02x %02x\n", debug_vals[0], debug_vals[1], debug_vals[2], debug_vals[3]);
 
         vuint16m1_t low_bytes = __riscv_vand_vx_u16m1(__riscv_vreinterpret_v_u8m1_u16m1(shuffled), 0x007F, vlmax_e16m1);
         vuint16m1_t high_bytes = __riscv_vand_vx_u16m1(__riscv_vreinterpret_v_u8m1_u16m1(shuffled), 0x7F00, vlmax_e16m1);
@@ -53,19 +46,52 @@ uint32_t varint_decode(uint8_t *input, uint32_t *output, size_t length)
 
         __riscv_vse32_v_u32m1(output, unpacked_result_a, 4);
 
-
         vuint32m1_t unpacked_result_b = __riscv_vsrl_vx_u32m1(__riscv_vreinterpret_v_u16m1_u32m1(packed_result), 16, vlmax_e32m1);
-        __riscv_vse32_v_u32m1(output+4, unpacked_result_b, 2);
-
-        		// __m128i unpacked_result_b = _mm_srli_epi32(packed_result, 16);
-		// _mm_storel_epi64(mout+1, unpacked_result_b);
-
-        // __m128i high_bytes_shifted = _mm_srli_epi16(high_bytes, 1);
-        // __m128i packed_result = _mm_or_si128(low_bytes, high_bytes_shifted);
-        // __m128i unpacked_result_a = _mm_and_si128(packed_result,
-        // _mm_set1_epi32(0x0000FFFF));
-        // _mm_storeu_si128(mout, unpacked_result_a);
+        __riscv_vse32_v_u32m1(output + 4, unpacked_result_b, 2);
+        return mask_result;
     }
 
-    return (uint32_t)result;
+    if (index < 145)
+    {
+        vuint8m1_t shuffled = __riscv_vrgather_vv_u8m1(varint_vec, __riscv_vreinterpret_v_i8m1_u8m1(vectors), vlmax_e8m1);
+
+        vuint32m1_t shuffled32 = __riscv_vreinterpret_v_u8m1_u32m1(shuffled);
+
+        vuint32m1_t low_bytes = __riscv_vand_vx_u32m1(shuffled32, 0x0000007F, vlmax_e32m1);
+        vuint32m1_t middle_bytes = __riscv_vand_vx_u32m1(shuffled32, 0x00007F00, vlmax_e32m1);
+        vuint32m1_t high_bytes = __riscv_vand_vx_u32m1(shuffled32, 0x007F0000, vlmax_e32m1);
+
+        vuint32m1_t middle_bytes_shifted = __riscv_vsrl_vx_u32m1(middle_bytes, 1, vlmax_e32m1);
+        vuint32m1_t high_bytes_shifted = __riscv_vsrl_vx_u32m1(high_bytes, 2, vlmax_e32m1);
+
+        vuint32m1_t low_middle = __riscv_vor_vv_u32m1(low_bytes, middle_bytes_shifted, vlmax_e32m1);
+        vuint32m1_t result = __riscv_vor_vv_u32m1(low_middle, high_bytes_shifted, vlmax_e32m1);
+        __riscv_vse32_v_u32m1(output, result, 4);
+        return mask_result;
+    }
+
+    // 	*ints_read = 2;
+
+    vuint8m1_t data_bits = __riscv_vand_vx_u8m1(varint_vec, 0x7F, vlmax_e8m1);
+    vuint8m1_t shuffled = __riscv_vrgather_vv_u8m1(data_bits, __riscv_vreinterpret_v_i8m1_u8m1(vectors), vlmax_e8m1); // passt
+
+    vuint64m1_t constant_vec = __riscv_vmv_v_x_u64m1(0x0010002000400080, vlmax_e64m1);
+    vuint16m1_t split_bytes = __riscv_vmul_vv_u16m1(__riscv_vreinterpret_v_u8m1_u16m1(shuffled), __riscv_vreinterpret_v_u64m1_u16m1(constant_vec), vlmax_e16m1); // passt
+
+    vuint64m1_t shifted_split_bytes = __riscv_vsll_vx_u64m1(__riscv_vreinterpret_v_u16m1_u64m1(split_bytes), 8, vlmax_e64m1); // passt
+
+    vuint64m1_t recombined = __riscv_vor_vv_u64m1(shifted_split_bytes, __riscv_vreinterpret_v_u16m1_u64m1(split_bytes), vlmax_e64m1); // passt
+
+    vuint64m1_t low_byte = __riscv_vsrl_vx_u64m1(__riscv_vreinterpret_v_u8m1_u64m1(shuffled), 56, vlmax_e64m1); // passt
+
+    vuint64m1_t result_evens = __riscv_vor_vv_u64m1(recombined, low_byte, vlmax_e64m1); // passt
+
+    uint8_t gather_index[] = {0, 2, 4, 6, 8, 10, 12, 14, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    vuint8m1_t gather_vec = __riscv_vle8_v_u8m1(gather_index, vlmax_e8m1);
+
+    vuint8m1_t result = __riscv_vrgather_vv_u8m1(__riscv_vreinterpret_v_u64m1_u8m1(result_evens), gather_vec, vlmax_e8m1);
+  
+    __riscv_vse32_v_u32m1(output, __riscv_vreinterpret_v_u8m1_u32m1(result), 2); // passt
+
+    return (uint32_t)mask_result;
 }
